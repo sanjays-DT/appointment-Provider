9b7b1f0afb0315468a7908a46d1c03117e8eee4d
Sanjay Saravanan <sanjays@datatemplate.com>
2026-02-11 14:29:31 +0530

    feat: created provider dashboard and unavailability pages UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { AuthProvider } from "@/context/AuthContext";
 import Navbar from "@/components/layout/Navbar";
+import { ThemeProvider } from "@/context/ThemeContext";
 
 export const metadata = {
   title: "Provider Dashboard",
@@ -13,11 +14,19 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="bg-slate-50 text-gray-900">
-        <AuthProvider>
-          <Navbar />
-          {children}
-        </AuthProvider>
+      <body
+        className="
+          bg-gray-100 text-gray-900
+          dark:bg-gray-950 dark:text-gray-100
+          transition-colors duration-300
+        "
+      >
+        <ThemeProvider>
+          <AuthProvider>
+            <Navbar />
+            {children}
+          </AuthProvider>
+        </ThemeProvider>
 
         <ToastContainer
           position="top-right"
