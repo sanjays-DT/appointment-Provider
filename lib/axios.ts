653db8955214787a6d64bf744e1fe960ebaf2a06
Sanjay Saravanan <sanjays@datatemplate.com>
2026-03-02 18:01:42 +0530

    updates axios.ts

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken } from "@/lib/auth";
 
-const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api";
+const NEXT_PUBLIC_API_BASE_URL = "http://localhost:5000/api";
 export const BASE_URL =  NEXT_PUBLIC_API_BASE_URL;
 
 const instance = axios.create({
